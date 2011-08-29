d9706bc15242e81b66363614f69d667aa40986ea
Kaitlin <katycorp@gmail.com>
2011-08-28 21:24:25 -0500

    initial commit

---
+++ rsvp/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.core.exceptions import ObjectDoesNotExist
+from rsvp.models import ATTENDING_CHOICES, Guest
+
+
+VISIBLE_ATTENDING_CHOICES = [choice for choice in ATTENDING_CHOICES if choice[0] != 'no_rsvp']
+
+
+class RSVPForm(forms.Form):
+    email = forms.EmailField()
+    name = forms.CharField(max_length=128)
+    attending = forms.ChoiceField(choices=VISIBLE_ATTENDING_CHOICES, initial='yes', widget=forms.RadioSelect)
+    number_of_guests = forms.IntegerField(initial=0)
+    comment = forms.CharField(max_length=255, required=False, widget=forms.Textarea)
+    
+    def __init__(self, *args, **kwargs):
+        if 'guest_class' in kwargs:
+            self.guest_class = kwargs['guest_class']
+            del(kwargs['guest_class'])
+        else:
+            self.guest_class = Guest
+        super(RSVPForm, self).__init__(*args, **kwargs)
+    
+    def clean_email(self):
+        try:
+            guest = self.guest_class._default_manager.get(email=self.cleaned_data['email'])
+        except ObjectDoesNotExist:
+            raise forms.ValidationError, 'That e-mail is not on the guest list.'
+        
+        if hasattr(guest, 'attending_status') and guest.attending_status != 'no_rsvp':
+            raise forms.ValidationError, 'You have already provided RSVP information.'
+        
+        return self.cleaned_data['email']
+    
+    def clean_number_of_guests(self):
+        if self.cleaned_data['number_of_guests'] < 0:
+            raise forms.ValidationError, "The number of guests you're bringing can not be negative."
+        return self.cleaned_data['number_of_guests']
+        
+    def save(self):
+        guest = self.guest_class._default_manager.get(email=self.cleaned_data['email'])
+        
+        if self.cleaned_data['name']:
+            guest.name = self.cleaned_data['name']
+        
+        guest.attending_status = self.cleaned_data['attending']
+        guest.number_of_guests = self.cleaned_data['number_of_guests']
+        guest.comment = self.cleaned_data['comment']
+        guest.save()
+        return guest
---
+++ urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', direct_to_template, {'template': 'index.html'}),
+    # Examples:
+    # url(r'^$', 'myproject.views.home', name='home'),
+    # url(r'^myproject/', include('myproject.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
