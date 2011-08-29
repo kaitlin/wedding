e56b4457d345c841ba0bf24115ac03d7e950c556
Kaitlin <kaitlin@kaitlin-netbook.(none)>
2011-08-29 00:53:13 -0400

    git ignroe and css changes

--- settings.py
+++ settings.py
@@ -49,11 +49,11 @@ MEDIA_URL = '/static/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = PROJECT_ROOT + '/static/staticfiles/'
+#STATIC_ROOT = PROJECT_ROOT + '/static/staticfiles/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -107,7 +107,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
     'rsvp',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
